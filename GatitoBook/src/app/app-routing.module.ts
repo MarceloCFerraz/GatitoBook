f4c14efcd32ab7cf2fa05227fd616d08b33dbbee
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2022-11-02 18:33:53 -0300

    implementada tela home e rotas c/ lazy loading para carregá-la e carregar o componente de login. Resta implementar a lógica para que seja possível fazer login

--- GatitoBook/src/app/app-routing.module.ts
+++ GatitoBook/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "home" },
+  {
+    path: "home",
+    loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
+    // Esta forma acima é o redirecionamento com lazy loading
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
